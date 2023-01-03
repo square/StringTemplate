4521e0403b9da0c8d5f1deb8498105d9e2a5f515
Adam Lickel <adam@lickel.com>
2023-01-03 13:02:44 -0800

    Change minimums to what Xcode 14 supports

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "StringTemplate",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_13),
+        .tvOS(.v11),
+        .watchOS(.v4),
+    ],
     products: [
         .library(
             name: "StringTemplate",
